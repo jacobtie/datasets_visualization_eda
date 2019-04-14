9d78d21c3621e469f0ee4ab4a34ba371921a2612
jacobtie <jacobtie@gmail.com>
2019-04-13 23:03:19 -0400

    Finished this version of the formatter and included a sample_datasets.json with 10 datasets

--- formatter.py
+++ formatter.py
@@ -9,6 +9,11 @@ if __name__ == '__main__':
 
     json_files = [f for f in os.listdir(file_path) if os.path.isfile(os.path.join(file_path, f))]
 
+    with open('badwords.txt', 'r') as bw:
+        bad_words = bw.read().split('\n')
+
+    errors = 0
+
     for f in json_files:
         try:
             with (open('template.json', 'r')) as template:
@@ -28,12 +33,33 @@ if __name__ == '__main__':
                     fileSize += data_file['dataFile']['filesize']
                 data_dict['filesize'] = fileSize
                 # Keywords
-                datasets.append(data_dict)
-                break
-        except Exception as ex:
-            print(f"I dunno an error or something\n{ex}")
-            break
+                keywords = []
 
+                for r in list(filter(lambda f: f['typeName'] == 'keyword', data_fields)):
+                    for k in r['value']:
+                        try:
+                            val = k['keywordValue']['value']
+                            if val not in bad_words and re.match('[0-9]+', val) is None:
+                                keywords.append(val)
+                        except:
+                            pass
+                        try:
+                            val = k['keywordVocabulary']['value']
+                            if val not in bad_words and re.match('[0-9]+', val) is None:
+                                keywords.append(val)
+                        except:
+                            pass
+                if keywords == []: continue
+                data_dict['keywords'] = keywords
+                datasets.append(data_dict)
+        except Exception as ex:
+            print(f'Error with file: {f}')
+            print(ex)
+            os.system(f'cp {file_path}/{f} {file_path}/Error')
+            errors += 1
+            continue
 
     with open('datasets.json', 'w') as o:
         json.dump(datasets, o)
+
+    print('datasets.json has been dumped')
